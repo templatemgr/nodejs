e3a6dc19505fb4188add9bd1ba0023bf581d8572
casjay <git-admin@casjaysdev.pro>
2024-06-02 15:09:49 -0400

    🗃️ Committing everything that changed 🗃️
    bin/node-server
    install.sh

--- install.sh
+++ install.sh
@@ -145,7 +145,6 @@ cat <<EOF | tee /etc/node/init_node.sh
 export NODE_MANAGER="$NODE_MANAGER" 
 export NODE_VERSION="$NODE_VERSION"
 if [ "\$NODE_MANAGER" = "fnm" ] && [ -n "\$(command -v fnm 2>/dev/null)" ]; then 
-  fnm use --install-if-missing \$NODE_VERSION && fnm default \$NODE_VERSION
   eval "\$(fnm env)" && eval "\$(fnm env --use-on-cd)" 
   echo "Node version is \$(node -v | sed 's|^v||g')"
 fi
