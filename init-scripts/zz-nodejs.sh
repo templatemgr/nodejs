062edcb972ec000d2de6b3caf08eda2744c559ab
casjay <git-admin@casjaysdev.pro>
2024-06-02 15:34:58 -0400

    🗃️ Committing everything that changed 🗃️
    init-scripts/zz-nodejs.sh

--- init-scripts/zz-nodejs.sh
+++ init-scripts/zz-nodejs.sh
@@ -117,8 +117,8 @@ SERVICE_PORT=""
 RUNAS_USER="root" # normally root
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # User and group in which the service switches to - IE: nginx,apache,mysql,postgres
-SERVICE_USER="nodejs"  # execute command as another user
-SERVICE_GROUP="nodejs" # Set the service group
+SERVICE_USER="daemon"  # execute command as another user
+SERVICE_GROUP="daemon" # Set the service group
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set user and group ID
 SERVICE_UID="0" # set the user id
